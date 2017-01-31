254d28d531612d6d2d7a0c3426af4dd977dc5de3
Jason Gresalfi <jason.gresalfi@gmail.com>
2017-01-30 18:24:15 -0600

    Added and tested filter function to remove duplicates

--- snippets.js
+++ snippets.js
@@ -4,18 +4,29 @@ var pageEls = Array.from(document.querySelectorAll('*'));
 
 function getColor(e) {
     let color = window.getComputedStyle(e, null)
-							          .getPropertyValue('background-color');
+        .getPropertyValue('background-color');
     return color;
 }
 
 function filterWhite(e) {
-	if (e !== 'rgba(0, 0, 0, 0)' && 'rgb(0, 0, 0, 0)') {
-		return e;
-	}
+    if (e !== 'rgba(0, 0, 0, 0)' && 'rgb(0, 0, 0, 0)') {
+        return e;
+    }
 }
 
-function killDupes() {}
+function killDupes(arr) {
+    let finalArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== (arr[i + 1])) {
+            finalArr.push(arr[i]);
+        }
+    }
+    return finalArr;
+}
 
-var colorArr = pageEls.map(getColor).filter(filterWhite);
+var colorArr = pageEls.map(getColor)
+    .filter(filterWhite)
+    .sort();
 
-console.log(colorArr);
+killDupes(colorArr);
+
